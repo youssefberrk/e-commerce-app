106ce0b8ad1254030284c03d2f0eea0dccce0d06
youssefberrk <yberrakouan.lol@gmail.com>
2026-02-04 13:42:37 +0100

    Add NavBar and Footer components; update layout and homepage with new structure and image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import Image from "next/image"
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    <div className=''>
+      <div className="relative aspect-[3/1] mb-12">
+    <Image
+    src="/featured.png" alt="Featured project" fill/>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import SearchBar from "./SearchBar";
+import { Bell, Home, ShoppingCart } from "lucide-react";
+
+const NavBar = () => {
+	return (
+		<nav
+			className="w-full flex items-center justify-between border-b border-gray-200 pb-4
+    ">
+			<Link href="/" className="flex items-center">
+				<Image
+					src="/logo.png"
+					alt="Logo"
+					width={36}
+					height={36}
+					className="w-6 h-6 md:w-9 md:h-9"
+				/>
+				<p className="hidden md:block text-md font-medium tracking-wider">
+					{" "}
+					E-Shop
+				</p>
+			</Link>
+			<div className="flex items-center gap-6">
+				<SearchBar />
+				<Link href="/">
+					<Home className="w-4 h-4 text-gray-600" />
+				</Link>
+				<Bell className="w-4 h-4 text-gray-600" />
+				<ShoppingCart className="w-4 h-4 text-gray-600" />
+                <Link href="/login">Sign In</Link>
+			</div>
+		</nav>
+	);
+};
+
+export default NavBar;
