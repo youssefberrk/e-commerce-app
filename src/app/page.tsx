88651a4896dadb2403af95e9a5759ccbdff7a775
youssefberrk <yberrakouan.lol@gmail.com>
2026-02-05 13:14:35 +0100

    Add product listing components and types; implement Categories and ProductCard with sample data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import Image from "next/image"
-const Homepage = () => {
-  return (
-    <div className=''>
-      <div className="relative aspect-[3/1] mb-12">
-    <Image
-    src="/featured.png" alt="Featured project" fill/>
-      </div>
-    </div>
-  )
-}
+import ProductList from "@/components/ProductList";
+import Image from "next/image";
 
-export default Homepage
+const Homepage = async ({
+	searchParams,
+}: {
+	searchParams: Promise<{ category: string }>;
+}) => {
+	const category = (await searchParams).category;
+	return (
+		<div className="">
+			<div className="relative aspect-[3/1] mb-12">
+				<Image src="/featured.png" alt="Featured Product" fill />
+			</div>
+			<ProductList category={category} params="homepage" />
+		</div>
+	);
+};
+
+export default Homepage;
