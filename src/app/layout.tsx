4501c08c84844c03c6489508ff3d15cdc9080e7b
youssefberrk <yberrakouan.lol@gmail.com>
2026-02-16 09:49:48 +0000

    Add ToastContainer for notifications in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import { ToastContainer } from "react-toastify";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
 					{children}
 					<Footer />
 				</div>
+				<ToastContainer position="bottom-right" />
 			</body>
 		</html>
 	);
